4859cd9c9fd2acf6c3db555c4053c6a4d2ae2653
lihaoran123 <545214926@qq.com>
2017-01-10 13:19:49 +0800

    由于马虎 程序文件名和内容不符，及时修改

--- calculator/Minus.java
+++ calculator/Minus.java
@@ -1,16 +1,12 @@
 /**
- *  ºı∑®¿‡
+ *  
  */
 
-public class Div extends AbstractCalculator implements ICalculator {
+public class Minus extends AbstractCalculator implements ICalculator {
     
     @Override
     public int calculate(String exp) {
-        int arrayInt[] = split(exp,"/");
-        if(arrayInt[1] != 0){
-            return arrayInt[0]/arrayInt[1];
-        }else{
-            return 0;
-        }
+        int arrayInt[] = split(exp,"-");
+        return arrayInt[0]-arrayInt[1];
     }
 }
